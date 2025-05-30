8e6bb4275fe007844ad17d5fa21161296e70164e
AngryCarrot <ilmarvvv@gmail.com>
2025-05-30 03:54:57 -0400

    Add project files.

---
+++ ChatAppTestAzure/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatAppTestAzure.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Text { get; set; }
+        public string Sentiment { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
